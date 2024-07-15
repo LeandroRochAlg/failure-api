6718bfa2c7712fa9032d915bb5b008da8b566b09
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-15 01:48:05 -0300

    feat: Add async keyword to UserController for managing user-related actions

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using failure_api.Models;
 using System.ComponentModel.DataAnnotations;
 
