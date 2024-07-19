5ccf0a735d56179643344bdf9dea5457f1f9ec3b
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-19 14:36:22 -0300

    feat: Update user description validation to limit length

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace failure_api.Models
@@ -10,6 +11,8 @@ namespace failure_api.Models
         public int Experience { get; set; } = 0;
         public bool Private { get; set; } = false;
         public bool Active { get; set; } = true;
+
+        [StringLength(300, ErrorMessage = "The {0} value cannot exceed {1} characters.")]
         public string Description { get; set; } = "";
         public string Link1 { get; set; } = "";
         public string Link2 { get; set; } = "";
